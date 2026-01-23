195b04faf9dbe1163cef48d85d68145ff4b891b9
Samyuktha <samyukthachelli12@gmail.com>
2026-01-23 10:03:56 +0530

    operators

---
+++ 4TH SEM/PYTHON/PRACTICE/M01_Language_and_Programming_Basics/S02/PS03_Operators.py
@@ -0,0 +1,25 @@
+#Bitwise Operators
+x = 7
+y = 13
+print(x&y)
+print(x^y)
+print(x|y)
+print(~x)
+print(x<<2)
+print(x>>2)
+
+#Membership Operators ==> in and not in
+print(100 in[10,20,30,40])
+print("on" in "python")
+
+
+#Identity Operators ==> is and is not
+x = 10
+y = 20
+z = 10
+print(x is y)
+print(x is z)
+
+list1 = [1,2,3]
+list2 = [1,2,3]
+print(list1 is list2)
