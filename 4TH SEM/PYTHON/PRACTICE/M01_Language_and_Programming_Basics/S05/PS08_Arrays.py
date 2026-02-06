63e9e0bcdc51c702721e5499a0f49a255165c968
Samyuktha <samyukthachelli12@gmail.com>
2026-02-06 12:26:16 +0530

    mes

---
+++ 4TH SEM/PYTHON/PRACTICE/M01_Language_and_Programming_Basics/S05/PS08_Arrays.py
@@ -0,0 +1,37 @@
+import numpy as np
+'''
+arr = np.array([10,20,30])
+print(arr)
+print(np.max(arr))
+print(np.min(arr))
+print(np.sum(arr))
+print(np.mean(arr))
+print("Even numbers list is:",np.arange(2,10,2))
+print("Odd numbers list is:",np.arange(1,10,2))
+n = int(input("Enter the size:"))
+ele=list(map(int,input("enter ele:").split()))
+print("Array Ele are:",np.array(ele))
+
+arr1 = np.array([12,13,14])
+arr2 = np.array([13,14,15])
+print(np.sum(arr1) + np.sum(arr2))
+
+nums = list(map(int,input("Enter nums:").split()))
+if len(set(nums))>3:
+    print(sorted(set(nums))[-3])
+else:
+    print(max(nums))
+
+nums = list(map(int,input("Enter nums:").split()))
+increasing = True
+decreasing = True 
+for i in range(1,len(nums)):
+    if nums[i] > nums[i-1]:
+        decreasing = False
+    if nums[i] < nums[i-1]:
+        increasing = False 
+if increasing or decreasing:
+    print("Monotonic Array")
+else:
+    print("Non Monotonic Array")
+'''
